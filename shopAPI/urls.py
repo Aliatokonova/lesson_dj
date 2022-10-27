0d09c95c4355ee59677c31b7797d7f6552f7596b
Woidless <bmoblp@mail.ru>
2022-10-27 11:18:50 +0600

    added swwager

--- shopAPI/urls.py
+++ shopAPI/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -10,11 +10,35 @@ from rest_framework.routers import SimpleRouter
 from category.views import CategoryViewSet
 from product.views import ProductViewSet
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 router = SimpleRouter()
 router.register('categories', CategoryViewSet)
 router.register('products', ProductViewSet)
 
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api/v1/accounts/', include('account.urls')),
